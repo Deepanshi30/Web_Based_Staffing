e42c9f21c4b4012e465d813d002e185be03cea1c
Deepanshi30 <deepanshimarch30@gmail.com>
2022-05-19 22:29:03 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Web_Based_Staffing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-IL6763JH;Initial Catalog=cmblogin;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from login where username='"+txtuser.Text+"' and password='"+txtpass.Text+"'",con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            string cmdItemValue =comboBox1.SelectedItem.ToString();
+            if(dt.Rows.Count > 0)
+            {
+                for(int i=0; i<dt.Rows.Count; i++)
+                {
+                    if (dt.Rows[i]["usertype"].ToString()==cmdItemValue)
+                    {
+                        MessageBox.Show("You are logged in as " + dt.Rows[i][2]);
+                        if (comboBox1.SelectedIndex == 0)
+                        {
+                            Form2 f2 =new Form2();
+                            f2.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            Form3 f3 = new Form3();
+                            f3.Show();
+                            this.Hide();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid");
+            }
+        }
+    }
+}
